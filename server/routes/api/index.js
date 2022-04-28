52a67eaa3ef5814fa3be926af3431c111905b25d
cosdaman <cosdaguy@gmail.com>
2022-04-28 14:11:30 -0700

    added more starter code to server

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,8 +1,4 @@
 const router = require('express').Router();
-const courseRoutes = require('./courseRoutes');
-const studentRoutes = require('./studentRoutes');
 
-router.use('/courses', courseRoutes);
-router.use('/students', studentRoutes);
 
 module.exports = router;
