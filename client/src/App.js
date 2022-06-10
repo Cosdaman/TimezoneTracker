3b4e2ae5fc9db72a4ff6aaa584a8f401b47e1d62
Cosdaman <kjc.ang@gmail.com>
2022-06-10 10:55:57 -0700

    working skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,27 @@
 import './App.css';
+import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <>
+      <Navbar />
+      <Routes>
+        <Route 
+          path="/" 
+          element={<Home />} 
+        />
+        <Route 
+          path="*" 
+          element={<h1 className="display-2">Wrong page!</h1>} 
+        />
+      </Routes>
+    </>
+  </Router>
   );
 }
 
---
+++ client/src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  Jumbotron,
+  Container,
+  CardColumns,
+  Card,
+  Button,
+} from 'react-bootstrap';
+
+const Home = () => {
+  return (
+    <>
+      <Container>
+Wao
+meido WIP
+      </Container>
+    </>
+  );
+};
+
+export default Home;
