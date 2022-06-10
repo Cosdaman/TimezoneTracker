8664491083044f2410326d1959cad6fcc5be332f
Cosdaman <kjc.ang@gmail.com>
2022-06-10 10:58:05 -0700

    actually push everything up now

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const db = require('./config/connection');
-const routes = require('./routes');
 const path = require('path');
 
 const PORT = process.env.PORT || 3001;
@@ -8,7 +7,6 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
